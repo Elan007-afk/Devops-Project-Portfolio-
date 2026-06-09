96cf126fc6fd485bb9b6796278d9f5a31713cbe8
DEMON <phoenixtamizh00@gmail.com>
2026-06-09 17:01:31 +0530

    Initial CI/CD project structure

---
+++ ci/cd-pipeline/terraform/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "app" {
+  name                 = "cicd-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# EKS Cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+}
+
+# VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+  name    = "cicd-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+}
